8d4763366e910061914df0cf14a137a8581a4d13
Galina Grigoryeva <repkakuku@gmail.com>
2017-01-08 18:55:12 +0200

    Added saving and loading from localStorage

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Шаблон задачи
 const taskTemplate = `
       <li class="list-group-item">
-        %TASK_TEXT%
+        <span class="task-text">%TASK_TEXT%</span>
         <div class="list-group__actions">
           <input type="checkbox" aria-label="...">
           <button type="submit" class="btn btn-default">Delete</button>
@@ -11,14 +11,75 @@ const taskTemplate = `
 var taskCreator = document.querySelector(".navbar-form--create-task-form");
 var taskList = document.querySelector(".list-group");
 
+const addTaskDOMElement = (description, completed) => {
+  //Создаём ДОМ элемент из шаблона
+  var template = taskTemplate;
+  template = template.replace("%TASK_TEXT%", description);
+
+  //div нужен для того, чтобы не добавлять класс к li (использовать innerHTML)
+  var tempDiv = document.createElement('div');
+  tempDiv.innerHTML = template;
+  var element = tempDiv.querySelector("li");
+  element.querySelector("input").checked = completed;
+
+  taskList.appendChild(element);
+
+  var checkbox = element.querySelector("input");
+
+  //Прослушиватель для кнопки удаления задачи
+  var deleteBtn = element.querySelector(".btn");
+
+  //Функция нужна для того, чтобы потом у неё удалить прослушиватели
+  const deleteBtnClickHandler = (event) => {
+    event.preventDefault();
+
+    taskList.removeChild(element);
+
+    deleteBtn.removeEventListener("click", deleteBtnClickHandler);
+    checkbox.removeEventListener("change", checkboxChangeHandler);
+
+    save();
+  }
+
+  deleteBtn.addEventListener("click", deleteBtnClickHandler);
+
+  const checkboxChangeHandler = (event) => {
+    save();
+  }
+
+  checkbox.addEventListener("change", checkboxChangeHandler);
+}
+
 //Загрузить данные из localStorage
 const load = () => {
+  var dataArrString = localStorage.getItem("data");
 
+  if (dataArrString) {
+    dataArr = JSON.parse(dataArrString);
+    dataArr.forEach((taskData) => {
+      addTaskDOMElement(taskData.description, taskData.completed);
+    });
+  }
 }
 
 //Сохранить данные в localStorage
 const save = () => {
+  var dataArr = [];
 
+  var taskElementsArr = document.querySelectorAll("li");
+  taskElementsArr.forEach((taskElement) => {
+    var description = taskElement.querySelector(".task-text").innerHTML;
+    var completed = taskElement.querySelector("input").checked;
+    var taskData = {
+      "description": description,
+      "completed": completed
+    };
+    dataArr.push(taskData);
+  });
+
+  var dataArrString = JSON.stringify(dataArr);
+
+  localStorage.setItem("data", dataArrString);
 }
 
 // Прослушиватель формы добавления задачи
@@ -34,31 +95,9 @@ taskCreator.addEventListener("submit", (event) => {
 
   input.value = "";
 
-  //Создаём ДОМ элемент из шаблона
-  var template = taskTemplate;
-  template = template.replace( "%TASK_TEXT%", inputValue );
-
-  //div нужен для того, чтобы не добавлять класс к li (использовать innerHTML)
-  var tempDiv = document.createElement('div');
-  tempDiv.innerHTML = template;
-  var element = tempDiv.querySelector("li");
-
-  taskList.appendChild(element);
+  addTaskDOMElement(inputValue, false);
 
   save();
-
-  //Прослушиватель для кнопки удаления задачи
-  var deleteBtn = element.querySelector(".btn");
-  //Функция нужна для того, чтобы потом у неё удалить прослушиватель
-  const deleteBtnClickHandler = (event) => {
-    event.preventDefault();
-
-    taskList.removeChild(element);
-    deleteBtn.removeEventListener("click", deleteBtnClickHandler);
-    save();
-  }
-
-  deleteBtn.addEventListener("click", deleteBtnClickHandler);
 });
 
 load();
